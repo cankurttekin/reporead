2cb2672a3a0a375a52954278e455a592c1819b3c
can kurttekin <cankurttekin@gmail.com>
2024-12-28 20:19:44 +0300

    backend init

---
+++ backend/src/main/java/com/reporead/reporead/ReporeadApplication.java
@@ -0,0 +1,13 @@
+package com.reporead.reporead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporeadApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/reporead/reporead/ReporeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reporead.reporead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
